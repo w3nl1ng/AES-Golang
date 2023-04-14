edce1c0e871364b28e3cca0b9396fdc9725e44dd
orange <3255464687@qq.com>
2023-04-14 17:49:48 +0800

    新增example演示代码

---
+++ example.go
@@ -0,0 +1,25 @@
+// This a example
+package main
+
+import (
+	"cryptoproj/mycrypto/aes"
+	"cryptoproj/mycrypto/util"
+	"fmt"
+)
+
+func main() {
+	plainText := "If you can't live longer, live deeper."
+	key := "0123456789abcdef"
+
+	fmt.Printf("\n[plainText]: %s\n", plainText)
+
+	plaintextInByte := []byte(plainText)
+	util.PKCS7Padding(&plaintextInByte, 16)
+
+	cipher := aes.Encrypto(plaintextInByte, []byte(key))
+	fmt.Printf("\n[cipher]: %v\n", cipher)
+
+	decryptoText := aes.Decrypto(cipher, []byte(key))
+	util.PKCS7UnPadding(&decryptoText)
+	fmt.Printf("\n[decryptoText]: %s\n", string(decryptoText))
+}
